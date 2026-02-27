54f39f33bab2b96ebe0bb3ba2b472e6f0c8050d0
kaan-erk <kaanerktoraman@gmail.com>
2026-02-27 14:52:53 +0300

    ex02 Finished

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,22 @@
-#include "Zombie.hpp"
 #include <iostream>
+#include <iomanip>
+
 
 int main(void)
 {
-    
+    std::string stringMain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &stringMain;
+    std::string &stringREF = stringMain;
+
+    std::cout << "----- Addresses ------\n";
+
+    std::cout << std::setw(25) << std::left << "Address of stringMain: " << &stringMain << std::endl;
+    std::cout << std::setw(25) << std::left << "Address of stringPTR: " << &stringPTR << std::endl;
+    std::cout << std::setw(25) << std::left << "Address of stringREF: " << &stringREF << std::endl;
+
+    std::cout << "----- Values ------\n";
+
+    std::cout << std::setw(25) << std::left << "Value of stringMain: " << stringMain << std::endl;
+    std::cout << std::setw(25) << std::left << "Value of stringPTR: " << (*stringPTR) << std::endl;
+    std::cout << std::setw(25) << std::left << "Value of stringREF: " << stringREF << std::endl;
 }
